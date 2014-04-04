302a35568add32e877b6c1e6ebd74a27cb3822c3
arkilic <arkilic@bnl.gov>
2014-04-03 22:25:23 -0400

    type added to ascan in propertyDepot

--- EXLog/propertyDepot/scans.py
+++ EXLog/propertyDepot/scans.py
@@ -1,11 +1,8 @@
 __author__ = 'arkilic'
-
+#TODO: Make session name a required attribute
 
 scan_list = ['Scan', 'ascan', 'hscan', 'kscan']
 scans = {'ascan' : {'name' : 'ascan', 'attributes': ['scan_id', 'start_timestamp', 'end_timestamp', 'descriptor', 'start',
-         'final','interval', 'descriptor', 'geometry']}
-
-
-        }
+         'final','interval', 'descriptor', 'geometry']}, 'type' : 'Scan'}
 
 
--- dummyBroker/Broker.py
+++ dummyBroker/Broker.py
@@ -9,15 +9,19 @@ class Broker():
     of configuration files will be read using Broker's parser. (Maybe pass all configuration to this module?)
 
     """
-    def __init__(self, client_name):
+    def __init__(self, session_name):
         try:
-            self.exlog_client = createLogInstance(client_name)
+            self.exlog_client = createLogInstance(session_name)
         except:
             raise
+        self.id = 0
         #Channelfinder client (if any) goes here
         #instruments can be controlled via another client
         #data broker uses 4 clients: EXLog, instrumentControl(?), channelfinder(?),
         #channel archiver. each client manages requests separately
+
+    def increase_id(self):
+        self.id += 1
 
     def create_property(self, property_name):
         if type(property_name) is list:
@@ -29,6 +33,8 @@ class Broker():
                     raise ValueError(str(entry) + 'property is not in the property template list')
     def capture(self,property_name,**kwds):
         #TODO: Add additional logic to capture() to avoid logging identical entries:all fields match perfectly
+        #TODO: Ensure the scan_id has not been used before.
+
         self.exlog_client.capture(property_name, **kwds)
 
     def log(self,description, owner=None, logbooks=[], tags=[], attachments=[],):
@@ -46,7 +52,6 @@ class Broker():
 
 
 scan = Broker('arman')
-
 scan.create_property(['ascan'])
 
 # scan.exlog_client.createProperty('arman33',['scan_id', 'start_timestamp', 'end_timestamp', 'descriptor', 'start', 'final', 'interval', 'descriptor', 'geometry'])
@@ -55,4 +60,6 @@ scan.create_property(['ascan'])
 scan.capture('ascan',scan_id=0)
 scan.capture('ascan',scan_id=1)
 scan.get_captured()
-scan.log(description='first log attempt using dummyBroker',logbooks=['CSX'])
+logbook = scan.exlog_client.get_Logbooks()[0]
+scan.log(description='first log attempt using dummyBroker',logbooks=[logbook])
+
